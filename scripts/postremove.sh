d73078bdc02429b4877d4f3fabc86ddc30ab39b3
Beni <braunger@zib.de>
2026-02-10 17:38:11 +0100

    added nfpm based rpm

---
+++ scripts/postremove.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+unlink /usr/bin/fusermount3
+mv /opt/fuzermount/fusermount3 /usr/bin/fusermount3
---
+++ scripts/preinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p /opt/fuzermount/
+mv /usr/bin/fusermount3 /opt/fuzermount/fusermount3
+ln -sf /opt/fuzermount/fuzermount /usr/bin/fusermount3
