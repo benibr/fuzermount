4be814188ec616eb45bb6ae8831fbf86294227d0
Beni <braunger@zib.de>
2026-02-18 11:40:46 +0100

    manage fusermount3 suid bit in rpm

--- scripts/postremove.sh
+++ scripts/postremove.sh
@@ -2,3 +2,4 @@
 
 unlink /usr/bin/fusermount3
 mv /opt/fuzermount/fusermount3 /usr/bin/fusermount3
+chmod +s /usr/bin/fusermount3
--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 mkdir -p /opt/fuzermount/
+
 mv /usr/bin/fusermount3 /opt/fuzermount/fusermount3
+chmod -s /opt/fuzermount/fusermount3
+
 ln -sf /opt/fuzermount/fuzermount /usr/bin/fusermount3
+
+chmod +s /opt/fuzermount/fuzermount
